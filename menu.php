a5efafad4538d30593609cac4d7f0dbf7d337ac7
vanessa <vanessa.chaillou@orange.fr>
2018-03-08 17:03:49 +0100

    correction fermeture <a>

--- menu.php
+++ menu.php
@@ -3,9 +3,9 @@
 	<div class="submenu">
 		<h3>Menu</h3>
 			<ul>
-				<li><a href="index.php" class="link1"/>Accueil</li>
-				<li><a href="article.php" class="link2"/>Articles</li>
-				<li><a href="contact.php" class="link3"/>Contact</li>	
+				<li><a href="index.php" class="link1"></a>Accueil</li>
+				<li><a href="article.php" class="link2"></a>Articles</li>
+				<li><a href="contact.php" class="link3"></a>Contact</li>	
 			</ul>
 	</div>
 </nav>
